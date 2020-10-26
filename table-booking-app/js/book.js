fc8d405009946bdbe406e055b3ed0693785a305e
Ryewan PC <ryewanpc@gmail.com>
2020-10-26 21:22:44 +0800

    add id to book and event listener

---
+++ table-booking-app/js/book.js
@@ -0,0 +1,34 @@
+function BookNow(guestName,guestEmail,guestPax) {
+    let url = 'https://api.sheety.co/7b082e81fa04098c0e0bfe8408decc01/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: guestName,
+            email: guestEmail,
+            pax: guestPax
+        }
+    } 
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      "Content-Type":"application/json"
+    }
+  })
+    .then((response) => response.json())
+    .then(json => {
+    // Do something with object
+        console.log(json.booking);
+        document.getElementById("bookMsg").innerHTML = json.booking.name + " sucessfully added!";
+        GetBookings();
+  });
+}
+
+document.getElementById("bookNow").addEventListener("click", function(){
+    let name = document.getElementById("guestName");
+    let email = document.getElementById("guestEmail");
+    let pax = document.getElementById("guestPax");
+    let remarks = document.getElementById("guestRemarks");
+
+    console.log(name + "," + email + "," + pax + "," + remarks);
+
+});
