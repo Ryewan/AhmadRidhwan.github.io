eac24c45d8e47c64d9846ed299778c47e8eab4ca
Ryewan PC <ryewanpc@gmail.com>
2020-10-30 22:31:22 +0800

    Update Vue.js

--- vue.js/scripts/index.js
+++ vue.js/scripts/index.js
@@ -9,5 +9,44 @@ var app2 = new Vue({
     el: "#app-2",
     data: {
         message: "You loaded this page on " + new Date().toLocaleString()
+        
     }
 });
+
+var app3 = new Vue ({
+    el: "#app-3",
+    data: {
+        seen: true
+    }
+});
+
+var app4 = new Vue ({
+    el: "#app-4",
+    data: {
+        todos: [
+            {text: "Learn HTml- Done"},
+            {text: "Learn CSS- Done"},
+            {text: "Learn JS- Done"},
+            {text: "Learn Vue- Done"}
+        ]
+    }
+});
+
+var app5 = new Vue ({
+    el: "#app-5",
+    data: {
+        message: "Ridhwan"
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
